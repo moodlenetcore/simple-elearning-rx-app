aaaf6402fc1c282ff702dadf9f702269428c3c97
Tan Le Anh <leanhtan911205@gmail.com>
2017-08-01 09:21:37 +0700

    Implementing...

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -105,41 +105,7 @@ const extractStyles = new ExtractTextPlugin({
 
 config.module.rules.push({
   test: /\.(sass|scss|css)$/,
-  loader: extractStyles.extract({
-    fallback: 'style-loader',
-    use: [
-      {
-        loader: 'css-loader',
-        options: {
-          sourceMap: project.sourcemaps,
-          minimize: {
-            autoprefixer: {
-              add: true,
-              remove: true,
-              browsers: ['last 2 versions'],
-            },
-            discardComments: {
-              removeAll : true,
-            },
-            discardUnused: false,
-            mergeIdents: false,
-            reduceIdents: false,
-            safe: true,
-            sourcemap: project.sourcemaps,
-          },
-        },
-      },
-      {
-        loader: 'sass-loader',
-        options: {
-          sourceMap: project.sourcemaps,
-          includePaths: [
-            inProjectSrc('styles'),
-          ],
-        },
-      }
-    ],
-  })
+  loader: 'style-loader!css-loader'
 })
 config.plugins.push(extractStyles)
 
